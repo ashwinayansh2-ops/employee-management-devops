d80dec6ed2bd13a6fcff2df472db2b83147ad4ce
shashi <ds_talluri@yahoo.com>
2026-07-23 04:05:42 +0530

    Task 04 - Configure React Router and application layout

--- app/frontend/src/components/Sidebar.jsx
+++ app/frontend/src/components/Sidebar.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 function Sidebar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside
+      style={{
+        width: "220px",
+        padding: "20px",
+        background: "#f4f4f4",
+      }}
+    >
+      <h3>Menu</h3>
+
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+
+        <li>
+          <Link to="/employees">Employees</Link>
+        </li>
+
+        <li>
+          <Link to="/add-employee">Add Employee</Link>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
 export default Sidebar;
