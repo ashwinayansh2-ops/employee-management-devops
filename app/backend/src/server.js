c3c9102ee0cce239e99eae707543aa2a899e6a38
shashi <ds_talluri@yahoo.com>
2026-08-06 16:25:50 +0530

    6th-14

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -32,7 +32,7 @@ app.get("/health", (req, res) => {
 
 app.use("/api/employees", employeeRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 
